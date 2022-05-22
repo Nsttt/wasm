5a41a46407ab669c370b234d577c42c9db1081f7
Nestor <elnestinatorHD@gmail.com>
2022-05-22 12:05:48 +0200

    Messing with State

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
 use yew::prelude::*;
 
-#[function_component(App)]
-fn app() -> Html {
-    html! {
-        <h1>{"Hello"}</h1>
+enum Msg {
+    AddOne,
+}
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let link = ctx.link();
+
+        html! {
+            <div>
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "Add One" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
     }
 }
 
 fn main() {
-    yew::start_app::<App>();
+    yew::start_app::<Model>();
 }
