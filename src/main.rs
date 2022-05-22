0c3b29e7a15922d8e72b84b5c11f3b569c051b53
Nestor <elnestinatorHD@gmail.com>
2022-05-22 11:48:24 +0200

    Working example

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
-use yew::prelude::*
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <h1>{"Hello"}</h1>
+    }
+}
+
+fn main() {
+    yew::start_app::<App>();
+}
